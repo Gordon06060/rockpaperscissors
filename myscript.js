95664c4431b0ca7f822473a4960338dd29682111
Gordon06060 <gordonz0913@gmail.com>
2024-04-24 23:37:16 -0700

    Finished Step 5:
    Finished creating function that allows players to play a single round.

--- myscript.js
+++ myscript.js
@@ -7,32 +7,70 @@ function getComputerChoice(){
     let randomNum = Math.floor(Math.random()*3);
 
     if (rock==randomNum){
-        console.log('Rock');
+        return ('rock');
     }
 
     else if(paper==randomNum){
-        console.log('Paper');
+        return ('paper');
     }
 
     else{
-        console.log('Scissors');
+        return ('scissors');
     }
-
-
 
 }
 
-getComputerChoice()
-
 // Step 3:
-
-
 function getHumanChoice(){
     let userInput = prompt('Enter your choice: Rock, Paper, or Scissors');
     return (userInput.toLowerCase());
+}
+
+
+
+// Step 4:
+let humanScore = 0;
+let computerScore = 0;
+
+
+//Step 5: 
+function playRound(humanChoice, computerChoice){
+    if ((humanChoice == 'rock') && (computerChoice == 'paper')){
+        console.log('You lose! Rock beats Paper ');
+        computerScore++;
+    } 
+
+    else if ((humanChoice == 'paper') && (computerChoice == 'rock')){
+        console.log('You win! Paper beats Rock')
+        humanScore++;
+    }
+
+    else if ((humanChoice == 'scissors') && (computerChoice == 'rock')){
+        console.log('You Lose! Rock Beats Scissors')
+        computerScore++;
+    }
+
+    else if ((humanChoice == 'rock') && (computerChoice == 'scissors')){
+        console.log('You win! Rock Beats Scissors')
+        humanScore++;
+    }
+
+    else if ((humanChoice == 'paper') && (computerChoice == 'scissors')){
+        console.log('You Lose! scissors beats paper')
+        computerScore++;
+    }
+
+    else if ((humanChoice == 'scissors') && (computerChoice == 'paper')){
+        console.log('You Win! Scissors beats paper')
+        humanScore++;
+    }
 
 }
 
-getHumanChoice();
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
 
